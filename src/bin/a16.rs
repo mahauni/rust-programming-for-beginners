010fc73a4cda28ec713b8fe94575b8f548b363d4
Lucas Raoni <lucasraoniha@gmail.com>
2022-11-27 13:57:57 -0300

    ex 16 option

--- src/bin/a16.rs
+++ src/bin/a16.rs
@@ -11,4 +11,35 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+struct Lockers {
+    name: String,
+    assign: Option<i32>
+}
+
+fn print(l: Lockers) {
+    match l.assign {
+        Some(i) => println!("{} has a locker at {}", l.name, i),
+        None => println!("{} doesnt has a locker", l.name)
+    }
+}
+
+fn main() {
+    let s: Vec<Lockers> = vec![
+        Lockers {
+            name: "Lucas".to_owned(),
+            assign: Some(14)
+        },
+        Lockers {
+            name: "Enrico".to_owned(),
+            assign: Some(21)
+        },
+        Lockers {
+            name: "Davi".to_owned(),
+            assign: None
+        },
+    ];
+
+    for i in s {
+        print(i)
+    }
+}
