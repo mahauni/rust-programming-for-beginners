6322427351181a596978251baa0b71b00649ee03
Lucas Raoni <lucasraoniha@gmail.com>
2022-11-28 17:06:57 -0300

    Result, error handler

--- src/bin/a18.rs
+++ src/bin/a18.rs
@@ -11,4 +11,39 @@
 // * Return a result from the function
 // * The Err variant should detail the reason why they cannot make a purchase
 
-fn main() {}
+struct Customer {
+    age: i32,
+    name: String
+}
+
+fn purchase(c: Customer) -> Result<String, String> {
+    if c.age > 21 {
+        Ok("You can purchase the item with success".to_owned())
+    } else {
+        Err("You can't buy the thing because you don't have the necessary age".to_owned())
+    }
+}
+
+fn main() {
+    let p1 = Customer {
+        age: 30,
+        name: "Lucas".to_owned()
+    };
+
+    let p2 = Customer {
+        age: 17,
+        name: "Davi".to_owned()
+    };
+
+    println!("Your purchase {}", p1.name);
+    match purchase(p1) {
+        Ok(m) => println!("{}", m),
+        Err(m) => println!("{}", m)
+    }
+
+    println!("Your purchase {}", p2.name);
+    match purchase(p2) {
+        Ok(m) => println!("{}", m),
+        Err(m) => println!("{}", m)
+    }
+}
--- src/bin/a18b.rs
+++ src/bin/a18b.rs
@@ -22,4 +22,77 @@
 // * Print whether the employee may access the building
 //   * Must use a function that utilizes the question mark operator to do this
 
-fn main() {}
+enum Employees {
+    MaintenanceCrews,
+    MarketingDepartmentEmployees,
+    Managers,
+    LineSupervisors,
+    KitchenStaff,
+    AssemblyTechnicians,
+}
+
+enum Status {
+    Active,
+    Terminated
+}
+
+struct Employee {
+    employee: Employees,
+    employed: Status
+}
+
+fn try_access(e: &Employee) -> Result<(), String> {
+    match e.employed {
+        Status::Terminated => return Err("Terminated".to_owned()),
+        _ => ()
+    }
+
+    match e.employee {
+        Employees::MaintenanceCrews => Ok(()),
+        Employees::MarketingDepartmentEmployees => Ok(()),
+        Employees::Managers => Ok(()),
+        _ => Err("Invalid Position".to_owned())
+    }
+}
+
+fn print_access(e: &Employee) -> Result<(), String> {
+    let _attempt_access = try_access(e)?;
+    println!("Access ok");
+    Ok(())
+}
+
+fn main() {
+    let e: Vec<Employee> = vec![
+        Employee {
+            employee: Employees::MaintenanceCrews,
+            employed: Status::Active
+        },
+        Employee {
+            employee: Employees::MarketingDepartmentEmployees,
+            employed: Status::Terminated
+        },
+        Employee {
+            employee: Employees::Managers,
+            employed: Status::Active
+        },
+        Employee {
+            employee: Employees::LineSupervisors,
+            employed: Status::Terminated
+        },
+        Employee {
+            employee: Employees::KitchenStaff,
+            employed: Status::Terminated
+        },
+        Employee {
+            employee: Employees::AssemblyTechnicians,
+            employed: Status::Active
+        }
+    ];
+
+    for i in e {
+        match print_access(&i) {
+            Err(e) => println!("Access denied: {}", e),
+            _ => ()
+        }
+    }
+}
