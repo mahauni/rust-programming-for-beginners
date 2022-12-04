ded3d49d34cb82fe3e51c4719955d704c7ba6920
Lucas Raoni <lucasraoniha@gmail.com>
2022-12-04 18:53:04 -0300

    new type pattern

--- src/bin/a28.rs
+++ src/bin/a28.rs
@@ -10,6 +10,7 @@
 // * Create a function for each type of clothes (shoes, shirt, pants)
 //   that accepts the new type specific to that type of clothing
 
+#[derive(Debug)]
 enum Color {
     Black,
     Blue,
@@ -23,4 +24,55 @@ enum Color {
     Yellow,
 }
 
-fn main() {}
+#[derive(Debug)]
+struct Shirt(Color);
+impl Shirt {
+    pub fn new(color: Color) -> Self {
+        Self(color)
+    }
+}
+
+#[derive(Debug)]
+struct Shoes(Color);
+impl Shoes {
+    pub fn new(color: Color) -> Result<Self, String> {
+        match color {
+            Color::Purple => Err("Purple is not available".to_owned()),
+            other => Ok(Self(other))
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Pants(Color);
+impl Pants {
+    pub fn new(color: Color) -> Self {
+        Self(color)
+    }
+}
+
+fn print_shoes (color: Shoes) {
+    println!("shoes color: {:?}", color)
+}
+
+fn print_pants (color: Pants) { 
+    println!("pants color: {:?}", color)
+}
+
+fn print_shirt (color: Shirt) {
+    println!("shirt color: {:?}", color)
+}
+
+fn main() {
+    let shirt_color = Shirt::new(Color::Gray);
+    let pants_color = Pants::new(Color::Blue);
+    let shoes_color = Shoes::new(Color::White);
+
+    print_pants(pants_color);
+    print_shirt(shirt_color);
+
+    match shoes_color {
+        Ok(shoes) => print_shoes(shoes),
+        Err(e) => println!("{}", e)
+    };
+}
